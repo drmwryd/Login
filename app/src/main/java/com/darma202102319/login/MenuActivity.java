45c8c323339f778490e0dda93a9eb46952426ffb
drmwryd <92357380+drmwryd@users.noreply.github.com>
2023-07-06 11:04:40 +0700

    Initial commit

---
+++ app/src/main/java/com/darma202102319/login/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.darma202102319.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
